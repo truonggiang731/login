1a3db39ccf44893da71a44ee14397258b0e00887
Nguyễn Giang <gnaigt@gmail.com>
2023-09-26 15:18:13 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ToggleComponent } from './toggle/toggle.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ToggleComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/toggle/toggle.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css'],
+})
+export class ToggleComponent implements OnInit {
+  @Input() checked = false;
+  @Output() checkedChange = new EventEmitter<boolean>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  toggle() {
+    this.checked = !this.checked;
+    this.checkedChange.emit(this.checked);
+  }
+}
